0afcff3686ce01fcfb6ac1ce3cc9ff5fac381211
Peter Elger <elger.peter@gmail.com>
2014-05-05 22:04:10 +0100

    added list commands

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./lib/config/config');
+var app = express();
+
+require('./lib/config/express')(app);
+require('./lib/routes')(app, config);
+app.listen(config.port, function () {
+  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+});
+
+exports = module.exports = app;
