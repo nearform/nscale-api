df8253dc85e5f43f9818172d756c3b4f98ea1e89
Peter Elger <elger.peter@gmail.com>
2014-05-06 13:49:18 +0100

    added socket.io

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var express = require('express');
+var express = require('express.io');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var config = require('./lib/config/config');
 var app = express();
+app.http().io();
 
 require('./lib/config/express')(app);
 require('./lib/routes')(app, config);
 app.listen(config.port, function () {
   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
 });
+
 
 exports = module.exports = app;
